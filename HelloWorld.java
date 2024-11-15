113bb7bfa772934e476f29e370402b702512ad34
dwg1207 <tranduong12072001@gmail.com>
2024-11-15 23:53:33 +0700

    cac ham xuat ra man hinh

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,15 @@
 package hoidanit;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Your name is: ");
+		String name = scanner.nextLine();
+		System.out.println("Your age is: ");
+		int age = scanner.nextInt();
+		System.out.println("My name is " + name + ", age = " + age);
+		scanner.close();
 	}
 }
